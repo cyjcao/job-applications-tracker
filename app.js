53b88b39601e88d00ff688a465f9c0b8aa78c147
cyjcao <johncy.cao@gmail.com>
2019-11-16 13:36:01 -0500

    version 0.2

--- app.js
+++ app.js
@@ -1,18 +1,162 @@
-const STATUS_OPTIONS = 4;
+const STATUS_VALUES = ['wishlist', 'applied', 'phone', 'site']; // job application status options (for radio buttons)
+const NUM_STATUS_OPTIONS = STATUS_VALUES.length;
+let wishlistItems = [];
 let applicationItems = [];
+let interviewItems = [];
+let itemsCreated = 0;
 
-function addApplication(company, position, link, status) {
-    const application = {
-        company,
-        position,
-        link,
-        status
-    };
 
-    applicationItems.push(application);
+// factory function that creates an application item object
+// company -> company name
+// position -> position title
+// link -> job posting url
+// status -> application status
 
-    const list = document.querySelector(".js-applications-list");
+function create_application_item(company, position, link=None, status) {
+    let id = company + '-' + position + itemsCreated;
+    return {company, position, link, status, id};
+}
+
+// show/hide the headers for each status list
+// hide when there's 0 items for a status
+function toggleHiddenListHeaders(){
+    const wishlistHeader = document.getElementById('wishlist-header');
+    const applicationsHeader = document.getElementById('applications-header');
+    const interviewsHeader = document.getElementById('interviews-header');
+    if(wishlistItems.length === 0 && !wishlistHeader.classList.contains('hidden')){
+        wishlistHeader.classList.add('hidden');
+    } else if(wishlistItems.length !== 0 && wishlistHeader.classList.contains('hidden')){
+        wishlistHeader.classList.remove('hidden');
+    }
+
+    if(applicationItems.length === 0 && !applicationsHeader.classList.contains('hidden')){
+        applicationsHeader.classList.add('hidden');
+    } else if(applicationItems.length !== 0 && applicationsHeader.classList.contains('hidden')){
+        applicationsHeader.classList.remove('hidden');
+    }
+
+    if(interviewItems.length === 0 && !interviewsHeader.classList.contains('hidden')){
+        interviewsHeader.classList.add('hidden');
+    } else if(interviewItems.length !== 0 && interviewsHeader.classList.contains('hidden')){
+        interviewsHeader.classList.remove('hidden');
+    }
+}
+
+function statusChangeHandler(event) {
+    const target = event.target;
+    const listItem = target.parentNode.parentNode.parentNode;
+    const listItemId = listItem.getAttribute('id');
+    console.log(listItem);
+    if(target.value === "wishlist"){
+        listItem.style.backgroundColor = "yellow";
+    } else if(target.value === "applied"){
+        listItem.style.backgroundColor = "orange";
+    } else if(target.value === "phone"){
+        listItem.style.backgroundColor = "#add8e6";
+    } else {
+        listItem.style.backgroundColor = "#009fab";
+    }
+
+    const wishlistElem = document.querySelector(".js-wishlist-list");
+    const applicationlistElem = document.querySelector(".js-applications-list");
+    const interviewlistElem = document.querySelector(".js-interviews-list");
+    if(target.value === STATUS_VALUES[0]){
+        if(!wishlistElem.contains(listItem) && applicationlistElem.contains(listItem)){
+          let oldChild = applicationlistElem.removeChild(listItem);
+          wishlistElem.appendChild(oldChild);
+
+          let application = applicationItems.find(item => item.id === listItemId);
+          applicationItems = applicationItems.filter(item => item.id !== listItemId);
+          wishlistItems.push(application);
+        } else if(!wishlistElem.contains(listItem) && interviewlistElem.contains(listItem)){
+            let oldChild = interviewlistElem.removeChild(listItem);
+            wishlistElem.appendChild(oldChild);
+  
+            let application = interviewItems.find(item => item.id === listItemId);
+            interviewItems = interviewItems.filter(item => item.id !== listItemId);
+            wishlistItems.push(application);
+        }
+    } else if(target.value === STATUS_VALUES[1]){
+        if(!applicationlistElem.contains(listItem) && wishlistElem.contains(listItem)){
+          let oldChild = wishlistElem.removeChild(listItem);
+          applicationlistElem.appendChild(oldChild);
+
+          let application = wishlistItems.find(item => item.id === listItemId);
+          wishlistItems = wishlistItems.filter(item => item.id !== listItemId);
+          applicationItems.push(application);
+        } else if(!applicationlistElem.contains(listItem) && interviewlistElem.contains(listItem)){
+            let oldChild = interviewlistElem.removeChild(listItem);
+            applicationlistElem.appendChild(oldChild);
+  
+            let application = interviewItems.find(item => item.id === listItemId);
+            interviewItems = interviewItems.filter(item => item.id !== listItemId);
+            applicationItems.push(application);
+        }
+    } else{
+        if(!interviewlistElem.contains(listItem) && wishlistElem.contains(listItem)){
+            let oldChild = wishlistElem.removeChild(listItem);
+            interviewlistElem.appendChild(oldChild);
+  
+            let application = wishlistItems.find(item => item.id === listItemId);
+            wishlistItems = wishlistItems.filter(item => item.id !== listItemId);
+            interviewItems.push(application);
+        } else if(!interviewlistElem.contains(listItem) && applicationlistElem.contains(listItem)){
+            let oldChild = applicationlistElem.removeChild(listItem);
+            interviewlistElem.appendChild(oldChild);
+  
+            let application = applicationItems.find(item => item.id === listItemId);
+            applicationItems = applicationItems.filter(item => item.id !== listItemId);
+            interviewItems.push(application);
+        }
+    }
+
+    toggleHiddenListHeaders();
+}
+
+function createStatusButtons(checked_value) {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'status-div');
+    const p = document.createElement('p');
+    p.textContent = 'Application Status:';
+    div.appendChild(p);
+    let radioArr = [];
+    for(let i = 0; i < NUM_STATUS_OPTIONS; i++){
+        const radioBtn = document.createElement('input');
+        radioBtn.setAttribute('type', 'radio');
+        radioBtn.setAttribute('name', 'status' + itemsCreated);
+        radioBtn.setAttribute('value', STATUS_VALUES[i]);
+        radioBtn.setAttribute('cursor', 'pointer');
+        if(radioBtn.getAttribute('value') === checked_value){
+            radioBtn.setAttribute('checked', true);
+        }
+
+        const label = document.createElement('label');
+        label.textContent = STATUS_VALUES[i][0].toUpperCase() + STATUS_VALUES[i].slice(1);
+        label.appendChild(radioBtn);
+        div.appendChild(label);
+
+        radioBtn.addEventListener('change', statusChangeHandler);
+    }
+
+    return div;
+}
+
+function addApplicationItem(company, position, link, status) {
+    const application = create_application_item(company, position, link, status);
+    let list;
+    if(application.status === STATUS_VALUES[0]){
+        wishlistItems.push(application);
+        list = document.querySelector(".js-wishlist-list");
+    } else if(application.status === STATUS_VALUES[1]) {
+        applicationItems.push(application);
+        list = document.querySelector(".js-applications-list");
+    } else{
+        interviewItems.push(application);
+        list = document.querySelector(".js-interviews-list");
+    }
+
     const listItem = document.createElement("li");
+    listItem.setAttribute("id", application.id);
     listItem.setAttribute("class", "application-item");
     const para1 = document.createElement("p");
     para1.textContent = application.company;
@@ -29,90 +173,20 @@ function addApplication(company, position, link, status) {
             listItem.style.backgroundColor = "orange";
             break;
         case "phone":
-            listItem.style.backgroundColor = "blue";
+            listItem.style.backgroundColor = "#add8e6";
             break;
         case "site":
-            listItem.style.backgroundColor = "green";
+            listItem.style.backgroundColor = "#009fab";
             break;
     }
-
-    const div = document.createElement("div");
-    div.setAttribute("class", "status-div");
-    div.style.fontSize = "12px";
-
-    const p = document.createElement("p");
-    p.textContent = "Application status:";
-    div.appendChild(p);
-
-    const radioGroupName = "job-status" + applicationItems.length;
-    for (let i = 0; i < STATUS_OPTIONS; i++) {
-        const radioInput = document.createElement("input");
-        radioInput.setAttribute("type", "radio");
-        radioInput.setAttribute("name", radioGroupName);
-        switch(i){
-            case 0:
-                radioInput.setAttribute("value", "wishlist");
-                radioInput.setAttribute("id", "wishlist" + applicationItems.length);
-                if (application.status === radioInput.value){
-                    radioInput.checked = true;
-                }
-                break;
-            case 1:
-                radioInput.setAttribute("value", "applied");
-                radioInput.setAttribute("id", "applied" + applicationItems.length);
-                if (application.status === radioInput.value){
-                    radioInput.checked = true;
-                }
-                break;
-            case 2:
-                radioInput.setAttribute("value", "phone");
-                radioInput.setAttribute("id", "phone" + applicationItems.length);
-                if (application.status === radioInput.value){
-                    radioInput.checked = true;
-                }
-                break;
-            case 3:
-                radioInput.setAttribute("value", "site");
-                radioInput.setAttribute("id", "site" + applicationItems.length);
-                if (application.status === radioInput.value){
-                    radioInput.checked = true;
-                }
-                break;
-        }
-        
-        const label = document.createElement("label");
-        label.setAttribute("for", radioInput.getAttribute("id"));
-        label.textContent = radioInput.value[0].toUpperCase() + radioInput.value.slice(1);
-
-        div.appendChild(radioInput);
-        div.appendChild(label);
-    }
     
-    listItem.appendChild(div);
+    const statusBtnDiv = createStatusButtons(application.status);
+    listItem.appendChild(statusBtnDiv);
 
     list.append(listItem);
-    console.log(listItem);
 
-    const radioGroup = document.querySelectorAll(`input[type=radio][name=${radioGroupName}]`);
-
-    Array.prototype.forEach.call(radioGroup, function(radio) {
-        radio.addEventListener('change', statusChangeHandler);
-     });
-}
-
-function statusChangeHandler(event) {
-    const target = event.target;
-    const listItem = target.parentNode.parentNode;
-    console.log(listItem);
-    if(target.value === "wishlist"){
-        listItem.style.backgroundColor = "yellow";
-    } else if(target.value === "applied"){
-        listItem.style.backgroundColor = "orange";
-    } else if(target.value === "phone"){
-        listItem.style.backgroundColor = "blue";
-    } else {
-        listItem.style.backgroundColor = "green";
-    }
+    toggleHiddenListHeaders();
+    itemsCreated += 1;
 }
 
 const form = document.querySelector(".js-form");
@@ -128,7 +202,7 @@ form.addEventListener("submit", function(event){
     const statusText = document.querySelector("input[name='job-status']:checked").value;
 
     if(companyText !== '' && positionText !== ''){
-        addApplication(companyText, positionText, linkText, statusText);
+        addApplicationItem(companyText, positionText, linkText, statusText);
 
         // reset the form
         companyInput.value = '';
